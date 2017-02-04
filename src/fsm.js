fd396586c2f51c28ad51ca3afa8bae72f1e7fe77
Krinitsyn Anton <Kr1an@hotmail.com>
2017-02-04 15:53:43 +0300

    test_50_persents

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,53 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(!config)
+            throw new Error();
+
+        this.states = config.states;
+        this.stack = [];
+        this.stack.push(config.initial);
+        this.initial = config.initial;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.stack[this.stack.length-1];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(this.states[state] == undefined)
+            throw new Error();
+        this.stack.pop();
+        this.stack.push(state);
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        var new_state;
+        new_state = this.states[this.stack[this.stack.length-1]].transitions[event];
+        if(new_state == undefined)
+            throw new Error();
+        this.stack.push(new_state);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.stack.push(this.initial);
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,14 +57,36 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if(event==undefined){
+            var keys = [];
+            for(var key in this.states)
+    	       keys.push(key);
+            return keys;
+        }else{
+            var values = [];
+            for(var key in this.states){
+                if(this.states[key].transitions[event]==undefined)
+                    continue;
+                values.push(key)
+            }
+            return values;
+        }
+
+
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(this.stack.length <= 1)
+            return false;
+        else
+            this.stack.pop();
+    }
 
     /**
      * Goes redo to state.
